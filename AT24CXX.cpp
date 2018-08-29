71999a4118644f80444f23ab408b24fe3e89f70d
Ali Abedi <30368346+xerophileshark@users.noreply.github.com>
2018-08-29 17:52:40 +0430

    Update AT24CXX.cpp

--- AT24CXX.cpp
+++ AT24CXX.cpp
@@ -1,7 +1,7 @@
 /*
   AT24CXX.h
   An Arduino library for the AT24 EEPROM Series.
-  Written by Ali Abedi for ROIT CO. August 21, 2018.
+  Written by Ali Abedi for ROIT CO. August 21, 2018. Updated in August 29, 2018.
   (http://roit.co/ali-abedi)
   The AT24 series of EEPROM are 5v level ICs which use I2C communication protocol to read and write data into them.
   MIT license, all text above must be included in any redistribution.
@@ -39,32 +39,35 @@ void AT24CXX::loadToRAM(int Size)
 
 uint8_t AT24CXX::readFrom(int address)
 {
-	if ( _loadedToRAM ) {
+	if ( _loadedToRAM && (address < _sizeOfSRAM) ) {
 		return _SRAM[address];
 	} else {
-		uint8_t tmpData = 0xFF;
-		Wire.beginTransmission(_EEPROM_I2C_ADDRESS);
-		Wire.write((int)(address >> 8));   // MSB
-		Wire.write((int)(address & 0xFF)); // LSB
-		Wire.endTransmission();
-		Wire.requestFrom(_EEPROM_I2C_ADDRESS, 1);
-		tmpData =  Wire.read();
-		return tmpData;
+		if (address < _size * 128) {
+			uint8_t tmpData = 0xFF;
+			Wire.beginTransmission(_EEPROM_I2C_ADDRESS);
+			Wire.write((int)(address >> 8));   // MSB
+			Wire.write((int)(address & 0xFF)); // LSB
+			Wire.endTransmission();
+			Wire.requestFrom(_EEPROM_I2C_ADDRESS, 1);
+			tmpData =  Wire.read();
+			return tmpData;
+		}
 	}
-	
 }
 
 void AT24CXX::writeTo(int address, uint8_t data)
 {
-	if ( _loadedToRAM ) {
+	if ( _loadedToRAM && (address < _sizeOfSRAM) ) {
 		_SRAM[address] = data;
 	} else {
-		Wire.beginTransmission(_EEPROM_I2C_ADDRESS);
-		Wire.write((int)(address >> 8));   // MSB
-		Wire.write((int)(address & 0xFF)); // LSB
-		Wire.write(data);
-		Wire.endTransmission();
-		delay(10); //write cycle delay
+		if (address < _size * 128) {
+			Wire.beginTransmission(_EEPROM_I2C_ADDRESS);
+			Wire.write((int)(address >> 8));   // MSB
+			Wire.write((int)(address & 0xFF)); // LSB
+			Wire.write(data);
+			Wire.endTransmission();
+			delay(10); //write cycle delay
+		}
 	}
 }
 
